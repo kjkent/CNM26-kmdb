2139db3717b443d80e1640c078e11da1398845a2
kjkent <kris@kriskent.com>
2021-08-25 08:15:56 +0100

    Implemented MongoDB

--- index.js
+++ index.js
@@ -1,28 +1,68 @@
-const fs = require("fs");
+require("dotenv").config();
+const mongoose = require("mongoose");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
-const { add, update, remove } = require("./utils");
 
-const app = () => {
-  let movieListArr;
-  try {
-    movieListArr = JSON.parse(fs.readFileSync("./movies.json"));
-  } catch (error) {
-    movieListArr = [];
-  }
+mongoose.connect(
+    `mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB connected");
+});
 
-  if (argv.add) {
-    add(movieListArr, argv.add);
-  } else if (argv.list) {
-    console.log(movieListArr);
-  } else if (argv.update && argv.with) {
-    update(movieListArr, argv.update, argv.with);
-  } else if (argv.remove) {
-    remove(movieListArr, argv.remove);
-  } else {
-    console.log("Usage: node index.js --add <film> / --list / --update <film> --with <newfilm> / --remove <film>");
-  }
+const Movie = mongoose.model(
+    "Movie",
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        year: {
+            type: Number
+        },
+        imdb: {
+            type: String
+        },
+        rt: {
+            type: String
+        },
+        comment: {
+            type: String,
+            default: "No comment added"
+        }
+    }
+);
+
+const app = async () => {
+    if (argv.add) {
+        const movie = new Movie({
+            title: argv.add,
+            year: argv.year,
+            imdb: argv.imdb,
+            rt: argv.rt,
+            comment: argv.comment
+        });
+        await movie.save();
+        console.log(movie);
+    } else if (argv.list) {
+        const list = await Movie.find({});
+        console.log(list);
+    } else if (argv.find) {
+        const result = await Movie.find({title: argv.find});
+        console.log(result);
+    } else if (argv.update && argv.comment) {
+        const updatedMovie = await Movie.updateOne({title: argv.update}, {comment: argv.comment});
+        console.log(updatedMovie);
+    } else if (argv.remove) {
+        const deletedMovie = await Movie.deleteOne({title: argv.remove});
+        console.log(deletedMovie);
+    } else {
+        console.log("Usage: node index.js --add <movie> / --list / --find <movie>/ --update <film> --comment <comment> / --remove <film>");
+    }
+    process.exit();
 };
 
 app();
