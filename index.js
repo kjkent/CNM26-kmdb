c40e49d09fa362c573a11afd1d6c97477f16b581
kjkent <kris@kriskent.com>
2021-08-26 22:12:15 +0100

    Integrated OMDb

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
+const fetch = require("node-fetch");
+
+require("dotenv").config();
 
 mongoose.connect(
     `mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
@@ -23,10 +25,13 @@ const Movie = mongoose.model(
         year: {
             type: Number
         },
-        imdb: {
+        runtime: {
             type: String
         },
-        rt: {
+        blurb: {
+            type: String
+        },
+        rtScore: {
             type: String
         },
         comment: {
@@ -36,16 +41,36 @@ const Movie = mongoose.model(
     }
 );
 
+const fetchFromOMDb = async () => {
+    const titleURI = encodeURIComponent(argv.add);
+    const response = await fetch(`http://www.omdbapi.com/?t=${titleURI}&apikey=${process.env.OMDB_API_KEY}`);
+    const json = await response.json();
+
+    const movieInfo = {
+        year: json.Year,
+        runtime: json.Runtime,
+        blurb: json.Plot,
+        rtScore: json.Ratings[1].Value
+    }
+
+    return movieInfo;
+}
+
 const app = async () => {
     if (argv.add) {
+        const movieInfo = await fetchFromOMDb();
+
         const movie = new Movie({
             title: argv.add,
-            year: argv.year,
-            imdb: argv.imdb,
-            rt: argv.rt,
+            year: movieInfo.year,
+            runtime: movieInfo.runtime,
+            blurb: movieInfo.blurb,
+            rtScore: movieInfo.rtScore,
             comment: argv.comment
         });
+
         await movie.save();
+        const movieID = movie._id;
         console.log(movie);
     } else if (argv.list) {
         const list = await Movie.find({});
